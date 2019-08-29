d940394fe896c9f1fa12eec4bbb927a3511e56db
Piotr <piotr.pawlk@gmail.com>
2019-08-29 11:43:11 +0200

    added tasks

---
+++ src/pl/pawluk/methods/Main14.java
@@ -0,0 +1,20 @@
+package pl.pawluk.methods;
+
+/* ### Zadanie 14
+
+Napisz program,który oblicza wynik dzielenia całkowitego bez reszty dówch liczb całkowitych a = 37 i b = 11;
+ */
+
+public class Main14 {
+
+    public static void main(String[] args) {
+
+        int a = 37;
+        int b = 11;
+
+        System.out.println("Program wyświetla wynik dzielenia całkowitego");
+        System.out.println("bez reszty dla dwóch liczb całkowitych.");
+        System.out.println("Dla liczb: a = " + a + ", b = " + b);
+        System.out.println(a + "/" + b + " = " + a/b + ".");
+    }
+}
